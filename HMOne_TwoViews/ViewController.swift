1e10ce346d2a03a8ffae084f55b17a3809744d5b
fattony01 <a.kondratenko.dev@gmail.com>
2023-05-24 20:39:39 +0200

    Initial Commit

---
+++ HMOne_TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMOne_TwoViews
+//
+//  Created by Anton Kondratenko on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
